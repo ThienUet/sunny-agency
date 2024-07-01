3dd8b68cd2990b84b51579d537549875c3373c70
ThienUet <thin102000@gmail.com>
2024-07-01 17:47:54 +0700

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  transpilePackages: ["antd", "@ant-design", "rc-util", "rc-pagination", "rc-picker", "rc-notification", "rc-tooltip", "rc-tree", "rc-table"],
   reactStrictMode: true,
 };
 
---
+++ src/config/AntDesignConfig.ts
@@ -0,0 +1,10 @@
+import type { ThemeConfig } from "antd";
+
+const AntdThemeConfig: ThemeConfig = {
+  token: {
+    fontSize: 16,
+    colorPrimary: "#52c41a",
+  },
+};
+
+export default AntdThemeConfig;
